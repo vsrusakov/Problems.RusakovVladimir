32f42829c1ad4ace517202a1939d882c3772c6ca
Vladimir Rusakov <117600863+vsrusakov@users.noreply.github.com>
2023-12-17 17:47:17 +0300

    Add files via upload

---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,127 @@
+﻿namespace QuickSort
+{
+    public class QuickSort
+    {
+        // быстрая сортировка с переключением на сортировку вставками и 
+        // выбором опорного элемента с помощью медианы из трех 
+
+        public const int small_array_size = 5;
+        public static void Sort<T>(T[] array, int left = 0, int right = -1) where T : IComparable<T>
+        {
+            if (right == -1) right = array.Length - 1;
+
+            if (right - left <= small_array_size - 1)
+            {
+                InsertionSort<T>(array, left, right);
+                return;
+            }
+            int p = Partition<T>(array, left, right);
+            Sort(array, left, p);
+            Sort(array, p + 1, right);
+        }
+        public static int Partition<T>(T[] array, int left, int right) where T : IComparable<T>
+        {
+            int median = left + (right - left) / 2;
+            T[] temp_array = { array[left], array[median], array[right] };
+            InsertionSort<T>(temp_array);
+            (array[left], array[median], array[right]) = (temp_array[0], temp_array[1], temp_array[2]);
+
+            int i = left, j = right;
+            T pivot = array[median];
+            
+            while (i <= j)
+            {
+                while (array[i].CompareTo(pivot) < 0) i++;
+
+                while (array[j].CompareTo(pivot) > 0) j--;
+
+                if (i >= j) break;
+
+                (array[i], array[j]) = (array[j], array[i]);
+                i++;
+                j--;
+            }
+            return j;
+        }
+        public static void InsertionSort<T>(T[] array, int left = 0, int right = -1) where T : IComparable<T>
+        {
+            if (right == -1) right = array.Length - 1;
+
+            for (int i = left + 1; i < right + 1; i++)
+            {
+                T temp = array[i];
+                int j = i - 1;
+                while ((j >= left) && (temp.CompareTo(array[j]) < 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
+        }
+        public static void Sort<T>(T[] array, IComparer<T> comparer, int left = 0, int right = -1)
+        {
+            if (right == -1) right = array.Length - 1;
+
+            if (right - left <= small_array_size - 1)
+            {
+                InsertionSort<T>(array, comparer, left, right);
+                return;
+            }
+            int p = Partition<T>(array, left, right, comparer);
+            Sort(array, comparer, left, p);
+            Sort(array, comparer, p + 1, right);
+        }
+        public static int Partition<T>(T[] array, int left, int right, IComparer<T> comparer)
+        {
+            int median = left + (right - left) / 2;
+            T[] temp_array = { array[left], array[median], array[right] };
+            InsertionSort<T>(temp_array, comparer);
+            (array[left], array[median], array[right]) = (temp_array[0], temp_array[1], temp_array[2]);
+
+            int i = left, j = right;
+            T pivot = array[median];
+
+            while (i <= j)
+            {
+                while (comparer.Compare(array[i], pivot) < 0) i++;
+
+                while (comparer.Compare(array[j], pivot) > 0) j--;
+
+                if (i >= j) break;
+
+                (array[i], array[j]) = (array[j], array[i]);
+                i++;
+                j--;
+            }
+            return j;
+        }
+        public static void InsertionSort<T>(T[] array, IComparer<T> comparer, int left = 0, int right = -1)
+        {
+            if (right == -1) right = array.Length - 1;
+
+            for (int i = left + 1; i < right + 1; i++)
+            {
+                T temp = array[i];
+                int j = i - 1;
+                while ((j >= left) && (comparer.Compare(temp, array[j]) < 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
+        }
+        public class MyComparer<T> : IComparer<T>
+            where T : IComparable<T>
+        {
+            private int count = 0;
+            public int Count { get { return count; } }
+            public int Compare(T? x, T? y)
+            {
+                count++;
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSortTest.cs
@@ -0,0 +1,126 @@
+﻿using Xunit;
+
+namespace QuickSort
+{
+    public class QuickSortTest
+    {
+        public static void PartitionTest()
+        {
+            QuickSort.MyComparer<int> int_cmp = new();
+
+            int[] array1 = { 9, 4, 10, -6, 14, 0, -11, -12, -7, 4, 7, -9, -12, 10, 11, -7, -10, 13, -13, -4 };
+            int[] array1_copy = (int[])array1.Clone();
+
+            int[] array2 = { 1, 0, 8, 1, 1, 3, 7, 7, 1, 6, 1, 9, 4, 7, 6 };
+            int[] array2_copy = (int[])array2.Clone();
+
+            // найдем опорный элемент для array1
+            int median1 = (array1.Length - 1) / 2;
+            int[] temp_array1 = { array1[0], array1[median1], array1[array1.Length - 1] };
+            QuickSort.InsertionSort<int>(temp_array1);
+            int pivot1 = temp_array1[1];
+
+            int p1 = QuickSort.Partition<int>(array1, 0, array1.Length - 1);
+            int p2 = QuickSort.Partition<int>(array1_copy, 0, array1.Length - 1, int_cmp);
+
+            Assert.Equal(p1, p2);
+            Assert.True(array1.SequenceEqual(array1_copy));
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (i <= p1)
+                    Assert.True(array1[i] <= pivot1);
+                else
+                    Assert.True(array1[i] >= pivot1);
+            }
+
+            // найдем опорный элемент для array2
+            int median2 = (array2.Length - 1) / 2;
+            int[] temp_array2 = { array2[0], array2[median2], array2[array2.Length - 1] };
+            QuickSort.InsertionSort<int>(temp_array2);
+            int pivot2 = temp_array2[1];
+
+            int q1 = QuickSort.Partition<int>(array2, 0, array2.Length - 1);
+            int q2 = QuickSort.Partition<int>(array2_copy, 0, array2.Length - 1, int_cmp);
+
+            Assert.Equal(q1, q2);
+            Assert.True(array2.SequenceEqual(array2_copy));
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (i <= q1)
+                    Assert.True(array2[i] <= pivot2);
+                else
+                    Assert.True(array2[i] >= pivot2);
+            }
+        }
+        public static void SortTest()
+        {
+            QuickSort.MyComparer<int> int_cmp1 = new();
+            QuickSort.MyComparer<int> int_cmp2 = new();
+            QuickSort.MyComparer<int> int_cmp1_default = new();
+            QuickSort.MyComparer<int> int_cmp2_default = new();
+
+            int[] array1 = { 9, 4, 10, -6, 14, 0, -11, -12, -7, 4, 7, -9, -12, 10, 11, -7, -10, 13, -13, -4 };
+            int[] array1_copy1 = (int[])array1.Clone();
+            int[] array1_copy2 = (int[])array1.Clone();
+
+            int[] array2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int[] array2_copy1 = (int[])array2.Clone();
+            int[] array2_copy2 = (int[])array2.Clone();
+
+            QuickSort.Sort<int>(array1_copy1);
+            QuickSort.Sort<int>(array1_copy2, int_cmp1);
+            Array.Sort(array1, int_cmp1_default);
+
+            QuickSort.Sort<int>(array2_copy1);
+            QuickSort.Sort<int>(array2_copy2, int_cmp2);
+            Array.Sort(array2, int_cmp2_default);
+
+            Assert.True(array1.SequenceEqual(array1_copy1));
+            Assert.True(array1.SequenceEqual(array1_copy2));
+
+            Assert.True(array2.SequenceEqual(array2_copy1));
+            Assert.True(array2.SequenceEqual(array2_copy2));
+
+            Assert.True(int_cmp1.Count > 0);
+            Assert.True(int_cmp2.Count > 0);
+
+            Console.WriteLine($"Кол-во сравнений для array1: {int_cmp1.Count}"); // 97
+            Console.WriteLine($"Кол-во сравнений для array2: {int_cmp2.Count}"); // 19
+
+            Console.WriteLine($"Кол-во сравнений для array1 при помощи Array.Sort(): {int_cmp1_default.Count}"); // 88
+            Console.WriteLine($"Кол-во сравнений для array2 при помощи Array.Sort(): {int_cmp2_default.Count}"); // 8
+        }
+        public static void InsertionSortTest()
+        {
+            QuickSort.MyComparer<int> int_cmp1 = new();
+            QuickSort.MyComparer<int> int_cmp2 = new();
+
+            int[] array1 = { 9, 4, 10, -6, 14, 0, -11, -12, -7, 4, 7, -9, -12, 10, 11, -7, -10, 13, -13, -4 };
+            int[] array1_copy1 = (int[])array1.Clone();
+            int[] array1_copy2 = (int[])array1.Clone();
+
+            int[] array2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int[] array2_copy1 = (int[])array2.Clone();
+            int[] array2_copy2 = (int[])array2.Clone();
+
+            QuickSort.InsertionSort<int>(array1_copy1);
+            QuickSort.InsertionSort<int>(array1_copy2, int_cmp1);
+            Array.Sort(array1);
+
+            QuickSort.InsertionSort<int>(array2_copy1);
+            QuickSort.InsertionSort<int>(array2_copy2, int_cmp2);
+            Array.Sort(array2);
+
+            Assert.True(array1.SequenceEqual(array1_copy1));
+            Assert.True(array1.SequenceEqual(array1_copy2));
+
+            Assert.True(array2.SequenceEqual(array2_copy1));
+            Assert.True(array2.SequenceEqual(array2_copy2));
+
+            Assert.True(int_cmp1.Count > 0);
+            Assert.True(int_cmp2.Count > 0);
+        }
+    }
+}
