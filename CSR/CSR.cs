f5f1b14c2bd2be9c755558fe35363b2267f360fb
Vladimir Rusakov <117600863+vsrusakov@users.noreply.github.com>
2023-12-22 19:39:23 +0300

    Add files via upload

---
+++ CSR/CSR.cs
@@ -0,0 +1,156 @@
+﻿using System.Numerics;
+
+namespace CSR
+{
+    public class CSR<T> : IEquatable<CSR<T>>
+        where T : INumber<T>
+    {
+        // Класс реализует представление CSR разреженной матрицы.
+        // Свойство Values хранит массив значений, свойство Indices 
+        // хранит массив индексов столбцов, свойство Indptr хранит
+        // массив индексации строк, свойство Shape хранит размеры матрицы.
+
+        public T[] Values { get; set; }
+        public int[] Indices { get; set; }
+        public int[] Indptr { get; set; }
+        public (int, int) Shape { get; set; }
+        public CSR(T[] values, int[] indices, int[] indptr, (int, int) shape)
+        {
+            Values = values;
+            Indices = indices;
+            Indptr = indptr;
+            Shape = shape;
+        }
+        public static CSR<T> ToCSR(T[,] dense_array)
+        {
+            // Метод строит CSR представление двумерного массива.
+
+            if (dense_array.Rank != 2) throw new ArgumentException("Матрица должна быть двумерной");
+
+            (int, int) shape = (dense_array.GetLength(0), dense_array.GetLength(1));
+            var values = new List<T>();
+            var indices = new List<int>();
+            int[] indptr = new int[shape.Item1 + 1];
+
+            for (int i = 0; i < shape.Item1; i++)
+            {
+                for (int j = 0; j < shape.Item2; j++)
+                {
+                    if (!T.IsZero(dense_array[i, j]))
+                    {
+                        values.Add(dense_array[i, j]);
+                        indices.Add(j);
+                    }
+                }
+                indptr[i + 1] = indices.Count;
+            }
+
+            return new CSR<T>(values.ToArray(), indices.ToArray(), indptr, shape);
+        }
+        public static T[,] ToDense(CSR<T> csr_array)
+        {
+            // Метод строит двумерный массив по его CSR представлению. 
+
+            T[,] dense_array = new T[csr_array.Shape.Item1, csr_array.Shape.Item2];
+            for (int i = 0; i < csr_array.Shape.Item1; i++)
+            {
+                int num_values = csr_array.Indptr[i + 1] - csr_array.Indptr[i];
+                for (int j = 0; j < num_values; j++)
+                {
+                    T value = csr_array.Values[csr_array.Indptr[i] + j];
+                    int k = csr_array.Indices[csr_array.Indptr[i] + j];
+                    dense_array[i, k] = value;
+                }
+            }
+            return dense_array;
+        }
+        public CSR<T> Transpose()
+        {
+            // Транспонирование матрицы в CSR представлении. 
+
+            (int, int) shape = (this.Shape.Item2, this.Shape.Item1);
+            int[] indptr = new int[shape.Item1 + 1];
+            List<int> new_indices = new();
+
+            for (int i = 0; i < this.Shape.Item1; i++)
+            {
+                int num_rows = this.Indptr[i + 1] - this.Indptr[i];
+                for (int j = 0; j < num_rows; j++)
+                {
+                    new_indices.Add(i);
+                    indptr[this.Indices[this.Indptr[i] + j] + 1]++;
+                }
+            }
+            var unsorted = this.Indices.Select((x, i) => new KeyValuePair<int, (int, T, int)>(x, (i, this.Values[i], new_indices[i])));
+            var sorted = from pair in unsorted
+                         orderby pair.Key, pair.Value.Item1
+                         select pair;
+
+            T[] values = sorted.Select(x => x.Value.Item2).ToArray();
+            int[] indices = sorted.Select(x => x.Value.Item3).ToArray();
+            for (int i = 1; i < indptr.Length; i++)
+            {
+                indptr[i] += indptr[i - 1];
+            }
+
+            return new CSR<T>(values, indices, indptr, shape);
+        }
+        public CSR<T> Dot(CSR<T> other)
+        {
+            // Умножение двух матриц в CSR представлении.
+
+            if (this.Shape.Item2 != other.Shape.Item1)
+                throw new Exception("Недопустимые размеры матриц");
+
+            (int, int) shape = (this.Shape.Item1, other.Shape.Item2);
+            List<T> values = new();
+            List<int> indices = new();
+            int[] indptr = new int[shape.Item1 + 1];
+
+            CSR<T> otherT = other.Transpose();
+
+            for (int i = 0; i < shape.Item1; i++)
+            {
+                for (int j = 0; j < shape.Item2; j++)
+                {
+                    T result = T.Zero;
+                    int p = this.Indptr[i], q = otherT.Indptr[j];
+
+                    while (p < this.Indptr[i + 1] && q < otherT.Indptr[j + 1])
+                    {
+                        if (this.Indices[p] > otherT.Indices[q])
+                            q++;
+                        else if (this.Indices[p] == otherT.Indices[q])
+                        {
+                            result += this.Values[p] * otherT.Values[q];
+                            p++; q++;
+                        }
+                        else
+                            p++;
+                    }
+
+                    if (!T.IsZero(result))
+                    {
+                        values.Add(result);
+                        indices.Add(j);
+                    }
+                }
+                indptr[i + 1] = values.Count;
+            }
+
+            return new CSR<T>(values.ToArray(), indices.ToArray(), indptr, shape);
+        }
+        public bool Equals(CSR<T>? other)
+        {
+            if (other is null) { return false; }
+
+            bool condition = this.Values.SequenceEqual(other.Values) &&
+                this.Indices.SequenceEqual(other.Indices) &&
+                this.Indptr.SequenceEqual(other.Indptr) &&
+                this.Shape.Equals(other.Shape);
+            return condition;
+        }
+        public override bool Equals(object? obj) => this.Equals(obj as CSR<T>);
+        public override int GetHashCode() => 0;
+    }
+}
---
+++ CSR/CSRTest.cs
@@ -0,0 +1,147 @@
+﻿using Xunit;
+
+namespace CSR
+{
+    public class CSRTest
+    {
+        public static int[,] mat1 = {
+            { 1, 2, 0, 3 },
+            { 0, 0, 4, 0 },
+            { 0, 1, 0, 11 }
+        };
+        public static double[,] mat2 = {
+            { 11.1, 22, 0, 0, 0 },
+            { 0, 33, 44, 0, 0},
+            { 0, 0, 55, 66.6, 77 },
+            { 0, 0, 0, 0, 0}
+        };
+        public static int[] mat1Values = { 1, 2, 3, 4, 1, 11 };
+        public static int[] mat1Indices = { 0, 1, 3, 2, 1, 3 };
+        public static int[] mat1Indptr = { 0, 3, 4, 6 };
+
+        public static double[] mat2Values = { 11.1, 22, 33, 44, 55, 66.6, 77 };
+        public static int[] mat2Indices = { 0, 1, 1, 2, 2, 3, 4 };
+        public static int[] mat2Indptr = { 0, 2, 4, 7, 7 };
+
+        public static void ToCSRTest()
+        {
+            // Проверим, что метод ToCSR верно определяет свойства
+            // Values, Indices и Indptr для матриц mat1 и mat2.
+
+            CSR<int> csr_mat1 = CSR<int>.ToCSR(mat1);
+            CSR<double> csr_mat2 = CSR<double>.ToCSR(mat2);
+
+            Assert.True(csr_mat1.Values.SequenceEqual(mat1Values));
+            Assert.True(csr_mat1.Indices.SequenceEqual(mat1Indices));
+            Assert.True(csr_mat1.Indptr.SequenceEqual(mat1Indptr));
+
+            Assert.True(csr_mat2.Values.SequenceEqual(mat2Values));
+            Assert.True(csr_mat2.Indices.SequenceEqual(mat2Indices));
+            Assert.True(csr_mat2.Indptr.SequenceEqual(mat2Indptr));
+        }
+        public static void ToDenseTest()
+        {
+            // Проверим, что метод ToDense правильно строит двумерный массив
+            // по его CSR представлению для матриц mat1 и mat2.
+
+            CSR<int> csr_mat1 = new(mat1Values, mat1Indices, mat1Indptr, (3, 4));
+            CSR<double> csr_mat2 = new(mat2Values, mat2Indices, mat2Indptr, (4, 5));
+
+            int[,] dense_mat1 = CSR<int>.ToDense(csr_mat1);
+            double[,] dense_mat2 = CSR<double>.ToDense(csr_mat2);
+
+            Assert.True(dense_mat1.Cast<int>().SequenceEqual(mat1.Cast<int>()));
+            Assert.True(dense_mat2.Cast<double>().SequenceEqual(mat2.Cast<double>()));
+        }
+        public static void TransposeTest()
+        {
+            // Проверим, что метод Transpose правильно строит CSR представление 
+            // при транспонировании и что соответствующий двумерный массив равен
+            // транспонированному исходному массиву.
+
+            int[,] mat1T = {
+                { 1, 0, 0 },
+                { 2, 0, 1 },
+                { 0, 4, 0 },
+                { 3, 0, 11 }
+            };
+            double[,] mat2T = {
+                { 11.1, 0, 0, 0 },
+                { 22, 33, 0, 0 },
+                { 00, 44, 55, 0 },
+                { 0, 0, 66.6, 0 },
+                { 0, 0, 77, 0}
+            };
+
+            CSR<int> csr_mat1 = new(mat1Values, mat1Indices, mat1Indptr, (3, 4));
+            CSR<double> csr_mat2 = new(mat2Values, mat2Indices, mat2Indptr, (4, 5));
+
+            csr_mat1 = csr_mat1.Transpose();
+            csr_mat2 = csr_mat2.Transpose();
+
+            CSR<int> csr_mat1T = CSR<int>.ToCSR(mat1T);
+            CSR<double> csr_mat2T = CSR<double>.ToCSR(mat2T);
+
+            Assert.True(csr_mat1.Equals(csr_mat1T));
+            Assert.True(CSR<int>.ToDense(csr_mat1).Cast<int>().SequenceEqual(mat1T.Cast<int>()));
+            Assert.True(CSR<int>.ToDense(csr_mat1T).Cast<int>().SequenceEqual(mat1T.Cast<int>()));
+
+            Assert.True(csr_mat2.Equals(csr_mat2T));
+            Assert.True(CSR<double>.ToDense(csr_mat2).Cast<double>().SequenceEqual(mat2T.Cast<double>()));
+            Assert.True(CSR<double>.ToDense(csr_mat2T).Cast<double>().SequenceEqual(mat2T.Cast<double>()));
+        }
+        public static void DotTest()
+        {
+            // Проверим, что нильпотеннтная матрица в квадрате равна нулевой матрице.
+
+            int[,] matNilpotent = {
+                { 5, -3, 2 },
+                { 15, -9, 6 },
+                { 10, -6, 4 }
+            };
+            var csr_matNilpotent = CSR<int>.ToCSR(matNilpotent);
+            var csr_matNilpotentSquared = csr_matNilpotent.Dot(csr_matNilpotent);
+
+            Assert.True(CSR<int>.ToDense(csr_matNilpotentSquared).Cast<int>().SequenceEqual(new int[9]));
+
+            // Умножение матриц размера (1, 3) и (3, 4).
+
+            int[,] vec = {
+                { 1 },
+                { 1 },
+                { 1 },
+            };
+
+            CSR<int> csr_mat1 = CSR<int>.ToCSR(mat1);
+            CSR<double> csr_mat2 = CSR<double>.ToCSR(mat2);
+            CSR<int> csr_vec = CSR<int>.ToCSR(vec).Transpose();
+
+            Assert.Throws<Exception>(() => csr_mat1.Dot(csr_vec));
+            Assert.True(CSR<int>.ToDense(csr_vec.Dot(csr_mat1)).Cast<int>().SequenceEqual(new int[] { 1, 3, 4, 14 }));
+
+            // Умножение двух матриц размера (3, 3)
+
+            int[,] m1 = {
+                { 5, 2, 1 },
+                { 4, 3, 2 },
+                { 2, 1, 5 }
+            };
+            int[,] m2 = {
+                { 1, 4, 3 },
+                { 2, 1, 5 },
+                { 3, 2, 1 }
+            };
+            int[,] m1xm2 = {
+                { 12, 24, 26 },
+                { 16, 23, 29 },
+                { 19, 19, 16 }
+            };
+
+            CSR<int> csr_m1 = CSR<int>.ToCSR(m1);
+            CSR<int> csr_m2 = CSR<int>.ToCSR(m2);
+            CSR<int> csr_m1xm2 = CSR<int>.ToCSR(m1xm2);
+
+            Assert.True(csr_m1.Dot(csr_m2).Equals(csr_m1xm2));
+        }
+    }
+}
